1c324fe0fca65799e34e1649f8803e3d9cf094de
Danilo Falcirolli <danilo.falcirolli5531@gmail.com>
2022-04-07 00:59:49 -0300

    danilo

---
+++ Exemplos/Variaveis/index.js
@@ -0,0 +1,40 @@
+// Aqui o exemplo para vocês, essa linha com o texto maior não precisa ser retirado o comentário, bons estudos. ( se tiverem hihi )
+
+// Exemplo da imagem 1 (LET)
+
+// let numero1 = 1;
+
+// function mostrarNumero(){
+//     console.log(numero1);
+// }
+
+// mostrarNumero();
+
+// Exemplo da imagem 2 (LET)
+
+// function numero(){
+//     let numero1 = 1;
+// }
+
+// console.log(numero1);
+
+// Exemplo da imagem 3 (LET)
+
+// function numero(){
+//     let numero1 = 1;
+//     console.log(numero1);
+// }
+// numero();
+
+// Exemplo da imagem 1 (VAR)
+
+// var numero1 = 1;
+// function mostrarNumero() {
+//     console.log(numero1);
+// }
+// mostrarNumero();
+
+// Exemplo da imagem 1 (Const)
+
+const numero1 = 1;
+numero1 = 3;

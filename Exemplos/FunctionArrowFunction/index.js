a6a382c97bc9db84727d3b1c4bf4bc8e789b6863
Danilo Falcirolli <danilo.falcirolli5531@gmail.com>
2022-04-11 22:28:17 -0300

    danilo

---
+++ Exemplos/FunctionArrowFunction/index.js
@@ -0,0 +1,45 @@
+// Aqui o exemplo para vocês, essa linha com o texto maior não precisa ser retirado o comentário, bons estudos. ( se tiverem hihi )
+
+// Exemplo da imagem 1 ( While )
+
+// var nomes = ["Giulia", "José", "Zézinho"];
+// var numero = 0;
+// while(numero != nomes.length){
+//     console.log(nomes[numero])
+//     numero++;
+// }
+
+// Exemplo imagem 1 ( For )
+
+// var nomes = ["Giulia", "José", "Zézinho"];
+// for(let numero = 0; numero < nomes.length; numero++){
+//     console.log(nomes[numero]);
+// }
+
+// Exemplo imagem 2.1 ( For in )
+
+// var nomes = ["Giulia", "José", "Zézinho"];
+// for(n in nomes){
+//     console.log(n)
+// }
+
+// Exemplo imagem 2.2 ( For in )
+
+// var nomes = ["Giulia", "José", "Zézinho"];
+// for(let n in nomes){
+//     console.log(nomes[n])
+// }
+
+// Exemplo imagem 3 ( For of )
+
+// var nomes = ["Giulia", "José", "Zézinho"];
+// for(let n of nomes){
+//     console.log(n)
+// }
+
+// Exemplo imagem 4 ( forEach )
+
+// var nomes = ["Giulia", "José", "Zézinho"];
+// nomes.forEach((item)=>{
+//     console.log(item)
+// })

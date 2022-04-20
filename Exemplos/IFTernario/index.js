231da01d7e8499acf8d02370ae282231876cc9c9
Danilo Falcirolli <danilo.falcirolli5531@gmail.com>
2022-04-20 19:04:00 -0300

    danilo

--- Exemplos/IFTernario/index.js
+++ Exemplos/IFTernario/index.js
@@ -1,6 +1,17 @@
 // Aqui o exemplo para vocês, essa linha com o texto maior não precisa ser retirado o comentário, bons estudos. ( se tiverem hihi )
 
-// IF aplicado dentro de uma função ( Imagem 1 )
-function soma(){
-    
-}
+// IF aplicado dentro de uma função, vendo se os numeros são iguais ( Imagem 1 )
+// function soma(num1, num2){
+//     if( num1 == num2 ){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// soma(1, 1);
+// soma(1, 2);
+
+// Ternario aplicado dentro de uma arrowfunction ( Imagem 2 )
+// var comparacao = (num1, num2) => { ( num1 == num2 ) ? console.log(true) : console.log(false) }
+// comparacao(1, 1);
+// comparacao(1, 2);

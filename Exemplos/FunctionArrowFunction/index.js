c7daf8fe501ad3ede125d9f6a22a26e9012ad5e6
Danilo Falcirolli <danilo.falcirolli5531@gmail.com>
2022-04-20 18:32:49 -0300

    danilo

--- Exemplos/FunctionArrowFunction/index.js
+++ Exemplos/FunctionArrowFunction/index.js
@@ -1,45 +1,19 @@
 // Aqui o exemplo para vocês, essa linha com o texto maior não precisa ser retirado o comentário, bons estudos. ( se tiverem hihi )
 
-// Exemplo da imagem 1 ( While )
-
-// var nomes = ["Giulia", "José", "Zézinho"];
-// var numero = 0;
-// while(numero != nomes.length){
-//     console.log(nomes[numero])
-//     numero++;
+// Função basica criada em JavaScript ( Imagem 1 )
+// function andar(){
+//     console.log("andei");
 // }
+// andar();
 
-// Exemplo imagem 1 ( For )
-
-// var nomes = ["Giulia", "José", "Zézinho"];
-// for(let numero = 0; numero < nomes.length; numero++){
-//     console.log(nomes[numero]);
+// Função atribuida a variavel ( Imagem 2 )
+// var correr = function(){
+//     console.log("estou correndo");
 // }
+// correr();
 
-// Exemplo imagem 2.1 ( For in )
-
-// var nomes = ["Giulia", "José", "Zézinho"];
-// for(n in nomes){
-//     console.log(n)
+// Arrow Function atribuida a variavel ( Imagem 3 )
+// var correr = () => {
+//     console.log("estou correndo");
 // }
-
-// Exemplo imagem 2.2 ( For in )
-
-// var nomes = ["Giulia", "José", "Zézinho"];
-// for(let n in nomes){
-//     console.log(nomes[n])
-// }
-
-// Exemplo imagem 3 ( For of )
-
-// var nomes = ["Giulia", "José", "Zézinho"];
-// for(let n of nomes){
-//     console.log(n)
-// }
-
-// Exemplo imagem 4 ( forEach )
-
-// var nomes = ["Giulia", "José", "Zézinho"];
-// nomes.forEach((item)=>{
-//     console.log(item)
-// })
+// correr();
